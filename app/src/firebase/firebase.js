3a93f3a028d6801f787c633fc3b9de8e18c47778
LoRaza <laurent.raza@gmail.com>
2018-06-18 11:21:49 +0200

    Test de connexion firebase, commit to merge

--- app/src/firebase/firebase.js
+++ app/src/firebase/firebase.js
@@ -3,35 +3,34 @@ import 'src/firebase/auth';
 
 
 const prodConfig = {
-    apiKey: "AIzaSyAqDMUFR0sO44aiuSK3xqBKErzmBVqFU4c",
-    authDomain: "construire-son-avenir.firebaseapp.com",
-    databaseURL: "https://construire-son-avenir.firebaseio.com",
-    projectId: "construire-son-avenir",
-    storageBucket: "",
-    messagingSenderId: "1058170086648"
-  };
+  apiKey: "AIzaSyAqDMUFR0sO44aiuSK3xqBKErzmBVqFU4c",
+  authDomain: "construire-son-avenir.firebaseapp.com",
+  databaseURL: "https://construire-son-avenir.firebaseio.com",
+  projectId: "construire-son-avenir",
+  storageBucket: "",
+  messagingSenderId: "1058170086648"
+};
 
 const devConfig = {
-    apiKey: "AIzaSyAqDMUFR0sO44aiuSK3xqBKErzmBVqFU4c",
-    authDomain: "construire-son-avenir.firebaseapp.com",
-    databaseURL: "https://construire-son-avenir.firebaseio.com",
-    projectId: "construire-son-avenir",
-    storageBucket: "",
-    messagingSenderId: "1058170086648"
-  };
+  apiKey: "AIzaSyAqDMUFR0sO44aiuSK3xqBKErzmBVqFU4c",
+  authDomain: "construire-son-avenir.firebaseapp.com",
+  databaseURL: "https://construire-son-avenir.firebaseio.com",
+  projectId: "construire-son-avenir",
+  storageBucket: "",
+  messagingSenderId: "1058170086648"
+};
 
-  const config = process.env.NODE_ENV === 'production'
+const config = process.env.NODE_ENV === 'production'
   ? prodConfig
   : devConfig;
-  
-  if (!firebase.apps.length) {
-    firebase.initializeApp(config);
-  }
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
+}
 
-  const auth = firebase.auth();
+const auth = firebase.auth();
 
-  export {
-    auth,
-  };
-  
-   export default firebase;
+export {
+  auth,
+};
+
+ export default firebase;
